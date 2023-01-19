3f938661ad01ee5dd23cdafd1fa70e3ce62d5f95
Divya Pujar <divyapujar0@gmail.com>
2023-01-19 10:13:43 +0530

    19-Jan-2023, improved jacoco code 71%

--- MemberManipulation/src/main/java/com/cg/membermanipulation/MemberManipulation/exception/CustomExceptionHandler.java
+++ MemberManipulation/src/main/java/com/cg/membermanipulation/MemberManipulation/exception/CustomExceptionHandler.java
@@ -11,28 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
-	
-	
-	
-		@ResponseStatus(HttpStatus.BAD_REQUEST)
-	    @ExceptionHandler(MethodArgumentNotValidException.class)
-	    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
-	        Map<String, String> errorMap = new HashMap<>();
-	        ex.getBindingResult().getFieldErrors().forEach(error -> 
-	            errorMap.put(error.getField(), error.getDefaultMessage())
-	        );
-	        return errorMap;
-	    }
-	
-	    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	    @ExceptionHandler(AddressNotValidException.class)
-	    public Map<String, String> handleAddressNotValidException(AddressNotValidException ex) {
-	        Map<String, String> errorMap = new HashMap<>();
-	        errorMap.put("errorMessage", ex.getMessage());
-	        return errorMap;
-	    }
-	    
-		
-	    
-	
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(AddressNotValidException.class)
+	public Map<String, String> handleAddressNotValidException(AddressNotValidException ex) {
+		Map<String, String> errorMap = new HashMap<>();
+		errorMap.put("errorMessage", ex.getMessage());
+		return errorMap;
+	}
+
 }
